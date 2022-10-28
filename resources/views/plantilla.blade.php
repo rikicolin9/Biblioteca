0663cb359bc77796ac62259a9f1cea28c4cf5eec
RICARDO COLIN MALDONADO <116596667+rikicolin9@users.noreply.github.com>
2022-10-28 18:31:57 -0500

    Inicio
    Se comenzó con la creacion del proyecto, una ves echo eso, defini las vistas que se necesitaban y la base de este que seria la plantilla.

---
+++ resources/views/plantilla.blade.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <script src="//cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+
+    <title>Reader Space</title>
+</head>
+<style>
+    .nav{
+      background-color:rgb(167, 168, 60);
+    }
+  
+  </style>
+</head>
+<body>
+
+<nav>
+<ul class="nav nav-tabs">
+  <li class="nav-item">
+    <a class="nav-link " href="#">Inicio</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link" href="#">Registro de Libros</a>
+  </li>
+</ul>
+</nav>
+@yield('content')
+
+<div class="alert alert-warning text-center" role="alert">
+Library Reader Space CopyRight©
+</div>
+
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+</body>
+</html>
